1ccc1903dbac1d8d900b2e7027f44933c8ad6cb4
android <email>
2023-02-02 12:26:25 -0500

    INtial Commit

---
+++ app/src/main/java/com/example/tipcal/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tipcal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity<EditView> extends AppCompatActivity {
+
+    private EditView billEditView
+    private EditView priceEditView
+    private button
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+   billEditView = findViewById(R.id.editText1);
+  priceEditView = finfViewById(R.id.editText2);
